8ce59a5020a14129418c9b1f9cad4e181d02ebd1
Alexandre Pellegrino <alexandre.pellegrino@simlabsdev.onmicrosoft.com>
2021-12-14 18:13:34 -0300

    desafio1

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,14 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    totalCount(list)
+  end
+
+  defp totalCount([]) do
+    0
+  end
+
+  defp totalCount(list) do
+    total = Enum.count(list)
+    total
   end
 end
